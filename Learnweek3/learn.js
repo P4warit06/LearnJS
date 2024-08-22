59457b519632346a2a9f0b0c01759856c7820417
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-08-22 11:00:36 +0700

    Update learn.js
    update

--- Learnweek3/learn.js
+++ Learnweek3/learn.js
@@ -1,3 +1,17 @@
-let score = 2;
-let unit = score >= 2 ? "points" : "point";
-console.log(`${score} ${unit}`);
+// let score = 2;
+// let unit = score >= 2 ? "points" : "point";
+// console.log(`${score} ${unit}`);
+
+//nullish the score
+// let nums //undefined
+// console.log(nums?.[0]) //undefined
+// let obj=null //null
+// console.log(obj?.id) //undefined
+
+
+
+// let nums
+// const values=nums ?? []
+// values[0]=555
+// console.log(values)//?
+
