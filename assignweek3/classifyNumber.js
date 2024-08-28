57c8177c0cb691ed17c2f37acc2a2eef95eaa101
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-08-28 20:51:17 +0700

    Update classifyNumber.js

--- assignweek3/classifyNumber.js
+++ assignweek3/classifyNumber.js
@@ -1,11 +1,8 @@
 function ClassifyNumber(num) {
-    if (num > 0) {
-        return 'positive';
-    } else if (num < 0) {
-        return 'negative';
-    } else {
-        return 'zero';
-    }
+   if(typeof num === 'undefined' || num == 'null') return undefined; 
+   else if (num > 0)  return 'positive';
+   if (num < 0)   return 'negative';
+   return 'zero';
 }
 console.log(ClassifyNumber(5)); // returns positive
 console.log(ClassifyNumber(-3)); // returns negative
