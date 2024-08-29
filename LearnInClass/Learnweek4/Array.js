c7e92bc9dabfc89e082722e66154497bf4baf128
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-08-29 10:30:55 +0700

    From week4 learn

---
+++ LearnInClass/Learnweek4/Array.js
@@ -0,0 +1,26 @@
+//Arrays Order have index order
+const values = [
+  123,
+  "Hello World ",
+  true,
+  [5, 6, 7],
+  { id: 1, title: "Untitled" } // object literal
+];
+values[values.length ] = 'goodbye world'
+console.log(values);
+console.log(values[0]); //first item 
+console.log(values[values.length-1]); // last item
+
+console.log('__________________ for each element');
+
+// loop to check in array
+values.forEach(element => {
+    console.log(element)    
+});
+
+console.log('============ for of  element');
+    
+
+for (const item of values ) {
+    console.log(item) 
+}
