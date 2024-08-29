2fbc6e096bc86044f897debe656876b9b635b522
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-08-29 11:41:29 +0700

    Update word.js

--- LearnInClass/Learnweek4/word.js
+++ LearnInClass/Learnweek4/word.js
@@ -1,5 +1,9 @@
-const words  = ['apple', 'book', 'car']
+const words = ['apple', 'book', 'car']
 
-for (let [index , word ] of object) {
-    
+for (let [index, word] of word.entries()) {
+  console.log(words, index)
+}
+
+for (const word of words) {
+  console.log(word)
 }
