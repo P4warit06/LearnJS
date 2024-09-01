2c5a5f30155331156bb53bb5fdef6fd4873bf251
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-01 23:02:41 +0700

    Update Datatype.js

--- LearnOutside/week1/Datatype.js
+++ LearnOutside/week1/Datatype.js
@@ -1,14 +1,19 @@
 let Name //สร้างตัวแปรไม่กำหนดค่า
-name = 'default' //  กำหนดข้อความลงในตัวแปร name = "default" ; //
+Name = 'default' //  กำหนดข้อความลงในตัวแปร name = "default" ; //
 let age = 20
 
 let name2 = 'ice',
   age2 = "30++ "; 
 
 var $for = "Is For test"; 
+// let สามารถ เขียนทับได้
+Name = "Not the name of the person you are looking for";
+
+const vat = 0.07 ; 
 
 console.log(Name)
 console.log(age)
 console.log(name2)
 console.log(age2)
 console.log($for);
+console.log(vat);
