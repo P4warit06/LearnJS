15793f25343938f8931ed193801e6dc6d45ffa7f
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-01 22:26:15 +0700

    Convers Type

---
+++ LearnOutside/week1/TypeConvers.js
@@ -0,0 +1,18 @@
+let money = 500 //number (integer)
+let price = 99.99.toString ; //number (float)
+let message = 'Ice love girls '
+let Name = 'Ice'
+
+let checkName = false
+
+
+let age = parseInt("20.15");
+let age2 = parseFloat("20.15");
+
+// console.log(typeof money)
+// console.log(typeof price)
+// console.log(typeof message)
+// console.log(typeof Name)
+// console.log(typeof checkName)
+console.log(typeof age2)
+console.log(price);
