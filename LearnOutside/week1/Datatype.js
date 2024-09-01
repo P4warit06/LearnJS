981bc5695cc7ddf99ed56786429c2f4abb1fedca
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-01 19:54:01 +0700

    First Datatype

---
+++ LearnOutside/week1/Datatype.js
@@ -0,0 +1,14 @@
+let Name //สร้างตัวแปรไม่กำหนดค่า
+name = 'default' //  กำหนดข้อความลงในตัวแปร name = "default" ; //
+let age = 20
+
+let name2 = 'ice',
+  age2 = "30++ "; 
+
+var $for = "Is For test"; 
+
+console.log(Name)
+console.log(age)
+console.log(name2)
+console.log(age2)
+console.log($for);
---
+++ LearnOutside/week1/app.js
@@ -0,0 +1,10 @@
+// แสดงข้อความในหน้าเว็บ
+document.write("Ice pawarit.com");
+document.write("<p> Javascript Basic</p>");
+alert("Hello"); 
+/*
+แสดงข้อความในแท็บ console browser
+*/ 
+console.log("hello again")
+console.error("Detect Virus ");
+console.warn("Detect Mistake");
