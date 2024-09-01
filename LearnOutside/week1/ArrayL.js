23b39d185ca4b339029c09332804a6b1144fb5e2
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-01 22:36:20 +0700

    Learn Array

---
+++ LearnOutside/week1/ArrayL.js
@@ -0,0 +1,28 @@
+let number1 = 1
+let number2 = 2
+let number3 = 3
+let number4 = 4
+let number5 = 5
+console.log(number1, '++', number2)
+console.log(number4)
+
+
+// let name Array = new Array();
+// let name Array = Array (mem1 , mem2 , mem3 , mem4) ;
+// Example
+// let myArray = new Array ();
+//MyArray[0] = 2000;
+// let days = Array  ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday) ;
+
+let number = Array (1,"2",3,4,5,6) ; 
+console.log(number);
+/*checkindex*/
+console.log("First Index : "+ number[0]);
+console.log("Second Index : "+ number[1]);
+
+//change in Array  
+let colors = ["black",'red', 'green', 'blue', 'yellow', ]
+colors[1] = "White";
+// outputs have changed in Array
+console.log(colors);
+
