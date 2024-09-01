782891c3d224b38d802776fb5fa54e967e433261
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-01 22:17:08 +0700

    DataType Part2

---
+++ LearnOutside/week1/Datatypep2.js
@@ -0,0 +1,15 @@
+let money = 500 ;  //number (integer)
+let price = 99.99 ; //number (float) 
+let message = "Ice love girls "; 
+let Name = "Ice" ; 
+
+let checkName = false ; 
+let age ="20" ;
+
+
+console.log(typeof (money) );
+console.log(typeof (price));
+console.log(typeof (message));
+console.log(typeof  (Name));
+console.log(typeof (checkName));
+console.log(typeof (age));
