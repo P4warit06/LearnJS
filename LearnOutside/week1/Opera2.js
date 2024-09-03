87d500ae4992d6679e1af089d29cdd04513becd7
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-03 22:34:44 +0700

    Part2 Operator

---
+++ LearnOutside/week1/Opera2.js
@@ -0,0 +1,25 @@
+let a= 5 , b =10 ;
+check1 = (a == b) ;
+check2 = (a != b );
+check3 = (a > b );
+check4 = (a < b );
+check5 = (a >= b );
+check6 = (a <= b );
+
+console.log(check5);
+console.log(check6);
+ 
+function checkGrade (score) { 
+if (score >= 70 ) return "A"
+if (score >= 50 && score < 70 ) return "B"
+if (score >= 30 && score < 50 ) return "C"
+if (score == 20 || score === 29) return "D"
+return "F" ; 
+}
+console.log(checkGrade(76));
+console.log(checkGrade(10));
+console.log(checkGrade(40));
+console.log(checkGrade(55));
+console.log(checkGrade(20));
+console.log(checkGrade(29));
+
