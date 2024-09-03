fc21cbfd963f824b397590bbe057eb4f712f05f0
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-03 22:21:50 +0700

    Part1 Operator

---
+++ LearnOutside/week1/Operator.js
@@ -0,0 +1,9 @@
+let a =10 ,b=20  ; 
+
+console.log("Sum", a+b);
+console.log("Minus", a-b);
+console.log("Time", a*b);
+console.log("Divide", a/b);
+console.log("Modulo",a%b);
+console.log("Exponent",a**b);
+
