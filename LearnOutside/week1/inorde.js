95987f9415a7b2f74c512960ae29463840d14581
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-03 23:30:24 +0700

    InorDe ค่า

--- LearnOutside/week1/inorde.js
+++ LearnOutside/week1/inorde.js
@@ -4,12 +4,29 @@ let a = 5,
   d = 8,
   e = 9
 
-// เพิ่มค่าแบบ Prefix
+// เพิ่มค่าแบบ Prefix เพิ่มก่อนแล้วนำไปใช้
 console.log('ค่าเริ่มต้น  =', a)
 console.log('PreFix = ', ++a)
 console.log('ค่าปัจจุบัน  =', a)
 
+console.log('-------------')
 
-// เพิ่มค่าแบบ Postfix 
-console.log('PostFix = ', a++)
-console.log(a)
+// เพิ่มค่าแบบ Postfix ใช้ก่อนแล้วค่อยเพิ่ม
+console.log('ค่าเริ่มต้น  =', b)
+console.log('PostFix = ', b++)
+console.log('ค่าปัจจุบัน  =', b)
+
+
+console.log('-------------------')
+
+// ลดค่าแบบ Prefix ลดก่อนแล้วนำไปใช้
+console.log('ค่าเริ่มต้น  =', a)
+console.log('PreFix = ', --a)
+console.log('ค่าปัจจุบัน  =', a)
+
+console.log('-------------------')
+
+// ลดค่าแบบ Postfix ใช้ก่อนแล้วค่อยลด
+console.log('ค่าเริ่มต้น  =', a)
+console.log('PreFix = ', a--)
+console.log('ค่าปัจจุบัน  =', a)
