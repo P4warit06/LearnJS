e240ccf8a18fbc30d02cf90e349598e61b159831
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-04 22:31:32 +0700

    Combine Array

---
+++ LearnInClass/assignweek4/CombineArray.js
@@ -0,0 +1,7 @@
+function combineArrays(arr1, arr2) {
+     return  arr1.concat(arr2);
+}
+
+console.log(combineArrays([1, 2], [3, 4]))
+console.log(combineArrays(['a', 'b'], ['c', 'd']))
+console.log(combineArrays([], [1, 2, 3]))
