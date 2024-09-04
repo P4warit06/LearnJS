cce9cac349afc73f3166fbe68b08e886597ab957
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-04 22:00:14 +0700

    Solve  pr1

---
+++ LearnInClass/assignweek4/turnStringToarr.js
@@ -0,0 +1,7 @@
+function getStringLengths(arr1) {
+  if (arr1 === undefined) return undefined
+    return arr1.map(str => str.length)
+}
+console.log(getStringLengths(['apple', 'banana', 'kiwi', 'orange'])) // 5 , 6 , 4  , 6
+console.log(getStringLengths(['hello', 'world'])) // 5 , 5
+console.log(getStringLengths(['Chatgpt', 'AI'])) // 7,  2 
