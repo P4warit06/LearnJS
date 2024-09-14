340a1b31d7da40fb8711f1ef36a0266103b5fc77
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-14 21:01:15 +0700

    update

---
+++ Practice/folder/removeFalsyValue.js
@@ -0,0 +1,6 @@
+const RemoveFalsyValue = (ArrName) =>  {
+        if (Array.isArray(ArrName) === undefined)  return  undefined; 
+        return ArrName.filter((a) => a  )
+
+}   
+console.log(RemoveFalsyValue([1,0,null,undefined,NaN,false,'',2,3]));
