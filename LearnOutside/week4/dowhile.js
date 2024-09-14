76abc542259077afce96bd4e1ef2b21dcab2f9d8
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-14 22:13:04 +0700

    Example Do while Loop

---
+++ LearnOutside/week4/dowhile.js
@@ -0,0 +1,18 @@
+let String  = 'I love you ';  
+let count = 0; 
+//ทำก่อนอย่างน้อย 1 รอบ
+// do{
+//  console.log(count);
+//  count++; 
+// } while(count <= 5); 
+
+/*while(count > 0){ 
+    console.log(count);
+    count++; 
+    if(count == 201 ) break; 
+} */
+while (true) { 
+    if(count === 200 ) break  
+    count++;
+    console.log(String);
+}
