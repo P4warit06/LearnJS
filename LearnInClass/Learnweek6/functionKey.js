c0d1785cada5e2b72597123d38fab0b8e5f99207
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-19 11:51:04 +0700

    check object key

---
+++ LearnInClass/Learnweek6/functionKey.js
@@ -0,0 +1,11 @@
+const square =  { 
+    id:1, 
+    side :4
+}
+const keys = Object.keys(square);
+const values = Object.values(square);
+const entries = Object.entries(square);
+console.log(keys);
+console.log(values);
+console.log(entries);
+
