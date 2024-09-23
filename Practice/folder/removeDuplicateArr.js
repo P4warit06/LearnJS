38a1de51cd1bb316242aac995477c90a0ce675a6
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-23 14:21:44 +0700

    Add
    add

--- Practice/folder/removeDuplicateArr.js
+++
@@ -1,5 +0,0 @@
-function removeDuplicates(arr) {
-  if (Array.isArray(arr)) return [...new Set(arr)]
-  /*ตรง Return Spread แล้วสร้าง Set ใหม่ ที่เก็บแค่ค่า  Unique */
-} 
-console.log(removeDuplicates([1, 2, 3, 3, 4]))
