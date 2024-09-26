d52784434cfd8100032c38fe41b50f752fcd65ed
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-26 09:59:19 +0700

    Add Code Comment From Friend

--- LearnInClass/assignweek6/ExtractAndRename.js
+++ LearnInClass/assignweek6/ExtractAndRename.js
@@ -1,8 +1,13 @@
 function ExtractAndRename (obj1){ 
-    const {name ,age} = obj1;
+    const  {name ,age} =obj1;
     const FullNameAndAge = {fullName:name,yearsOld:age};
-    return FullNameAndAge;
+    return FullNameAndAge
+    // teacher 
+    // const {name : fullName, age : yearolds} =  obj1 
+    //console.log(fullName);
+    //console.log(yearsOld);
+    // return {fullName ,yearsOld} 
+    
 }
-
 console.log(ExtractAndRename({name:'John',age:25,city:'New York'}));
 console.log(ExtractAndRename({name:'Jane',age:30}));
