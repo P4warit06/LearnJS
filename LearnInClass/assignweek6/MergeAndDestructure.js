314953babf70743ae5aa77e4dea8fa608403676b
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-09-26 10:00:52 +0700

    Add Code Example From Friend

--- LearnInClass/assignweek6/MergeAndDestructure.js
+++ LearnInClass/assignweek6/MergeAndDestructure.js
@@ -1,7 +1,19 @@
 function mergeAndDestructure(obj1, obj2) {
-    const {name,age} = { ...obj1, ...obj2 };
-    return {name,age};
-
+  const { name, age } = { ...obj1, ...obj2 }
+  return { name, age }
 }
-console.log(mergeAndDestructure({name:'John', age:25},{city:'New York'}))
-console.log(mergeAndDestructure({name:'Jane'},{age:30,country:'USA'}))
+console.log(
+  mergeAndDestructure({ name: 'John', age: 25 }, { city: 'New York' })
+)
+console.log(mergeAndDestructure({ name: 'Jane' }, { age: 30, country: 'USA' }))
+/* const mergeAndDestracture = (obj1, obj2) => {
+  const mearge = { ...obj1, ...obj2 } sprea 
+  return { name:mearge.name, age:mearge.age } not destructure use object.key
+}  Nay*/
+
+/*function mergeAndDestructure(obj1, obj2) {
+  let cloneObj = { ...obj1, ...obj2 } || Spread 
+  let { name, age } = cloneObj || Destructure 
+  return { name, age }
+  tae 
+}*/
