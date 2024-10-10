64ee68c2e1f61e6124c00e3f82f367732af0ee14
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-10-10 11:50:11 +0700

    Update Function

--- LearnInClass/Learn/Learnweek9/function.js
+++ LearnInClass/Learn/Learnweek9/function.js
@@ -10,4 +10,10 @@ console.log(y(10)) // what is a result of y() ? , 10
 console.log(typeof y) // function
 console.log(echo === y) // result  should be A true
 
-// compose function เอา Function มาประกอบร่างกันเพื่อแก้สิ่งใดสิ่งหนึ่ง
+/* compose function เอา Function มาประกอบร่างกันเพื่อแก้สิ่งใดสิ่งหนึ่ง
+Higher Order Function be take other function as argument or return them self 
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6]
+const doubleEvenNumbers = numbers.filter(num => num % 2 === 0  ).map(num => num *2 ) 
+console.log(doubleEvenNumbers);
