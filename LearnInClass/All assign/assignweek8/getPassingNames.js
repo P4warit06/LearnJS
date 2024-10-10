20b1e4e5b59f55316eea8822a604205c9abbe54f
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-10-10 09:29:25 +0700

    Update Solution & more way  Solution

--- LearnInClass/All assign/assignweek8/getPassingNames.js
+++ LearnInClass/All assign/assignweek8/getPassingNames.js
@@ -1,15 +1,28 @@
 const studentScores = [
- {name :'Alice' ,score : 85} , 
- {name :'Bob' ,score : 92} , 
- {name :'Charlie' ,score : 68} , 
- {name :'David' ,score : 55} , 
- {name :'Eve' ,score : 78} , 
-] ; 
+  { name: 'Alice', score: 85 },
+  { name: 'Bob', score: 92 },
+  { name: 'Charlie', score: 68 },
+  { name: 'David', score: 55 },
+  { name: 'Eve', score: 78 },
+]
+// Declarative  program - what i want
 const getPassingNames = (StudentNewArray) => {
-  return StudentNewArray
-    .filter(studentScores => studentScores.score >= 70)
-    .map (studentScores => studentScores.name.toUpperCase()) 
+  return StudentNewArray.filter(
+    (studentScores) => studentScores.score >= 70
+  ).map((studentScores) => studentScores.name.toUpperCase()) //แปลงค่า Array
 }
+// function Chaining
+const passingNames = getPassingNames(studentScores)
+console.log(passingNames)
 
-const passingNames = getPassingNames (studentScores)
-console.log(passingNames);
+/*
+function getPassingNames(students) {
+  const passingNames = []
+  for (const student of students) {
+    if (student.score >= 70) {
+      passingNames.push(student.name.toUpperCase())
+    }
+  }
+  return passingNames
+}
+*/
