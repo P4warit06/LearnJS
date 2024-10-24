ecedce5883088be5d153c2acb6df96809259d1f3
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-10-24 15:30:23 +0700

    Update ExtractAndRename.js

--- LearnInClass/All assign/asw6/ExtractAndRename.js
+++ LearnInClass/All assign/asw6/ExtractAndRename.js
@@ -1,13 +1,13 @@
-function ExtractAndRename (obj1){ 
-    const  {name ,age} =obj1;
-    const FullNameAndAge = {fullName:name,yearsOld:age};
+function ExtractAndRename(obj) {
+    const {name ,age} = obj 
+    const  FullNameAndAge = { fullname : name , yearsOlds:  age}
     return FullNameAndAge
-    // teacher 
-    // const {name : fullName, age : yearolds} =  obj1 
-    //console.log(fullName);
-    //console.log(yearsOld);
-    // return {fullName ,yearsOld} 
-    
 }
-console.log(ExtractAndRename({name:'John',age:25,city:'New York'}));
-console.log(ExtractAndRename({name:'Jane',age:30}));
+// teacher
+// const {name : fullName, age : yearolds} =  obj1
+//console.log(fullName);
+//console.log(yearsOld);
+// return {fullName ,yearsOld}
+
+console.log(ExtractAndRename({ name: 'John', age: 25, city: 'New York' }))
+console.log(ExtractAndRename({ name: 'Jane', age: 30 }))
