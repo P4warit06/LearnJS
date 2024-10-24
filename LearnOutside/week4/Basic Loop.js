c9cda158030432b9a2c323e2b45e89f689305860
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-10-24 15:29:15 +0700

    Update Basic Loop.js

--- LearnOutside/week4/Basic Loop.js
+++ LearnOutside/week4/Basic Loop.js
@@ -1,13 +1,18 @@
-let count = 5 // assign variable count is 1 จำนวนนับรอบ
-/*while (count < 3 ) { 
-   console.log("Hello world!"); //1 ,2 
-   count++;  // 1=> 2   
-}*/
+// let count = 1 // assign variable count is 1 จำนวนนับรอบ
+// /*while (count < 3 ) { 
+//    console.log("Hello world!"); //1 ,2 
+//    count++;  // 1=> 2   
+// }*/
+// while (count <= 5) { // เงื่อนไขยังเป็นจริงอยู่
+//   console.log('ผลิตสินค้าชิ้นที่ = ', count)
+//   if (count == 4) {
+//     break
+//   }
+//   count++
+// }/
+function  greeting(str1, str2) { 
+      return `Meet ${str2} And She want to say ${str1}!`
 
-while (count <= 5) {
-  console.log('ผลิตสินค้าชิ้นที่ = ', count)
-  if (count == 4) {
-    break
-  }
-  count++
 }
+console.log(greeting("Hello" , "Anna"));
+console.log(greeting("Hi" , "Aulora"));
