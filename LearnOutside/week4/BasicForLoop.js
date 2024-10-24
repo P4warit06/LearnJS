81016d09c14c49a48bad0d021e5d5a0d0c2c5553
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-10-24 15:27:46 +0700

    Update BasicForLoop.js

--- LearnOutside/week4/BasicForLoop.js
+++ LearnOutside/week4/BasicForLoop.js
@@ -4,10 +4,12 @@
 for (let count1 = 10; count1 >= 1; count1--) {
   console.log(count1)
 }*/
-let count = 1
-do{
-    console.log(count);
-}while (count > 5) {
+let count = 10
+do {
+  if (count == 0) break
   console.log(count)
+  count--
+} while (true)
+{
+  console.log('ERROR: ' + count)
 }
-
