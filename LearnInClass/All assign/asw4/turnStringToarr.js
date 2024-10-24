4a5d9990e8e890745e1740ba4af24bd366fdb5c0
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-10-24 15:31:23 +0700

    Update turnStringToarr.js

--- LearnInClass/All assign/asw4/turnStringToarr.js
+++ LearnInClass/All assign/asw4/turnStringToarr.js
@@ -1,8 +1,11 @@
 function getStringLengths(arr1) {
-  if (typeof arr1 === undefined) return undefined
-    return arr1.map(str => str.length)
+  if ( arr1 === undefined) return undefined
+  if (!Array.isArray(arr1)) return null 
+  return arr1.map(str => str.length)
 }
 console.log(getStringLengths(['apple', 'banana', 'kiwi', 'orange'])) // 5 , 6 , 4  , 6
 console.log(getStringLengths(['hello', 'world'])) // 5 , 5
 console.log(getStringLengths(['Chatgpt', 'AI'])) // 7,  2 
+console.log(getStringLengths(undefined))
+console.log(getStringLengths('String')) 
  
