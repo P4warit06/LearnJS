7d15735e6619dfd5fd762fd3c047ce06be6cf9aa
Pawarit Samriang <142296227+P4warit06@users.noreply.github.com>
2024-10-24 15:27:35 +0700

    Update user.js
    update some user

--- Tutor/week2/user.js
+++ Tutor/week2/user.js
@@ -17,54 +17,54 @@
     },
   }
 
-  function getUserPreferences(userProfile) {
-    const userName = userProfile?.name ?? 'Guest'
-    const userAge = userProfile?.age ?? undefined
-    const userEmail = userProfile?.preferences?.notifications?.email
-      ? 'Email notifications enabled'
-      : 'Email notifications disabled'
-    return { userName, userAge, userEmail }
-  }
-  console.log(getUserPreferences(userProfile))
-}
+//   function getUserPreferences(userProfile) {
+//     const userName = userProfile?.name ?? 'Guest'
+//     const userAge = userProfile?.Age ?? '18'
+//     const userEmail = userProfile?.preferences?.notifications ? 
+//      "Email notifications enabled" :
+//     "Email notifications disabled" 
+//     return { userName, userAge, userEmail }
+//   }
+//   console.log(getUserPreferences(userProfile))
+// }
 
 //Math practice
-{
-  console.log('=================')
-  console.log('Math practice')
+// {
+//   console.log('=================')
+//   console.log('Math practice')
 
-  function rollDice() {
-    return Math.floor(Math.random() * 6) + 1
-  }
-  console.log(rollDice()) //should return a random number between 1 and 6
+//   function rollDice() {
+//     return Math.floor(Math.random() * 6) + 1
+//   }
+//   console.log(rollDice()) //should return a random number between 1 and 6
 
-  function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * max + min - 1) + min
-  }
-  console.log(generateRandomNumber(1, 100))
-}
+//   function generateRandomNumber(min, max) {
+//     return Math.floor(Math.random() * max + min - 1) + min
+//   }
+//   console.log(generateRandomNumber(1, 100))
+// }
 
 //Conditional practice
-{
-  console.log('=================')
-  console.log('Conditional practice')
+// {
+//   console.log('=================')
+//   console.log('Conditional practice')
 
-  function categorizeAge(age) {
-    return age <= 13
-      ? 'child'
-      : age <= 19
-      ? 'teen'
-      : age <= 59
-      ? 'adult'
-      : 'senior'
-  }
+//   function categorizeAge(age) {
+//     return age <= 13
+//       ? 'child'
+//       : age <= 19
+//       ? 'teen'
+//       : age <= 59
+//       ? 'adult'
+//       : 'senior'
+//   }
 
-  // Example Calls:
-  console.log(categorizeAge(10)) // Output: child
-  console.log(categorizeAge(16)) // Output: teen
-  console.log(categorizeAge(35)) // Output: adult
-  console.log(categorizeAge(70)) // Output: senior
-}
+//   // Example Calls:
+//   console.log(categorizeAge(10)) // Output: child
+//   console.log(categorizeAge(16)) // Output: teen
+//   console.log(categorizeAge(35)) // Output: adult
+//   console.log(categorizeAge(70)) // Output: senior
+// }
 
 //For loop example and practice
 {
@@ -73,10 +73,10 @@
   const arr = [1, 2, 3]
 
   for (const n in arr) {
-    console.log('forin', n)
+    console.log('for in', n)
   }
   for (const n of arr) {
-    console.log('forof', n)
+    console.log('for of', n)
   }
   arr.forEach((n) => console.log('foreach', n))
 
@@ -98,7 +98,7 @@
   console.log('Comparing practice')
 
   function areObjectsEqual(obj1, obj2) {
-        return  obj1.compareTo(obj2)  ? true : false 
+    return obj1.compareTo(obj2) ? true : false
   }
 
   const obj1 = { name: 'John', age: 30 }
@@ -110,163 +110,163 @@
 }
 
 //Array
-{
-  console.log('=================')
-  console.log('Array')
+// {
+//   console.log('=================')
+//   console.log('Array')
 
-  //Array literals
-  {
-    console.log('1.Array literals')
+//   //Array literals
+//   {
+//     console.log('1.Array literals')
 
-    const numbers = [1, 2, 3, 4, 5] // Basic Array of Numbers
-    const fruits = ['apple', 'banana', 'cherry'] // Array of Strings
-    const mixedArray = [42, 'hello', true, null, { name: 'John' }] // Mixed Data Types
-    const nestedArray = [
-      [1, 2],
-      [3, 4],
-      [5, 6],
-    ] // Nested Arrays
-    const emptyArray = [] // Empty Array
-    const users = [
-      // Array of Objects
-      { id: 1, name: 'Alice' },
-      { id: 2, name: 'Bob' },
-      { id: 3, name: 'Charlie' },
-    ]
-    const variedArray = [
-      true,
-      'text',
-      123,
-      null,
-      undefined,
-      { key: 'value' },
-      [1, 2, 3],
-    ] // Array with Various Data Types
-    console.log(numbers)
-    console.log(fruits)
-    console.log(mixedArray)
-  }
+//     const numbers = [1, 2, 3, 4, 5] // Basic Array of Numbers
+//     const fruits = ['apple', 'banana', 'cherry'] // Array of Strings
+//     const mixedArray = [42, 'hello', true, null, { name: 'John' }] // Mixed Data Types
+//     const nestedArray = [
+//       [1, 2],
+//       [3, 4],
+//       [5, 6],
+//     ] // Nested Arrays
+//     const emptyArray = [] // Empty Array
+//     const users = [
+//       // Array of Objects
+//       { id: 1, name: 'Alice' },
+//       { id: 2, name: 'Bob' },
+//       { id: 3, name: 'Charlie' },
+//     ]
+//     const variedArray = [
+//       true,
+//       'text',
+//       123,
+//       null,
+//       undefined,
+//       { key: 'value' },
+//       [1, 2, 3],
+//     ] // Array with Various Data Types
+//     console.log(numbers)
+//     console.log(fruits)
+//     console.log(mixedArray)
+//   }
 
-  //Spread operator
-  {
-    console.log('2.Spread operator')
+//   //Spread operator
+//   {
+//     console.log('2.Spread operator')
 
-    const originalArray = [1, 2, { name: 'Alice' }] // originalArray contains a number, a number, and an object
+//     const originalArray = [1, 2, { name: 'Alice' }] // originalArray contains a number, a number, and an object
 
-    // Create a new array using the spread operator
-    const newArray = [...originalArray]
+//     // Create a new array using the spread operator
+//     const newArray = [...originalArray]
 
-    console.log(originalArray) // Output: [1, 2, { name: "Alice" }]
-    console.log(newArray) // Output: [1, 2, { name: "Alice" }]
+//     console.log(originalArray) // Output: [1, 2, { name: "Alice" }]
+//     console.log(newArray) // Output: [1, 2, { name: "Alice" }]
 
-    // Check if they are the same reference
-    console.log(originalArray === newArray) // Output: false
+//     // Check if they are the same reference
+//     console.log(originalArray === newArray) // Output: false
 
-    // Modify an object in the original array
-    originalArray[2].name = 'Bob'
-    originalArray[1] = 35
+//     // Modify an object in the original array
+//     originalArray[2].name = 'Bob'
+//     originalArray[1] = 35
 
-    console.log(originalArray) // Output: [1, 35, { name: "Bob" }]
-    console.log(newArray) // Output: [1, 2, { name: "Bob" }]  // Notice that object it also changed in newArray
+//     console.log(originalArray) // Output: [1, 35, { name: "Bob" }]
+//     console.log(newArray) // Output: [1, 2, { name: "Bob" }]  // Notice that object it also changed in newArray
 
-    // Deep copy
-    {
-      console.log('Deep copy')
+//     // Deep copy
+//     {
+//       console.log('Deep copy')
 
-      const oArr = [1, 2, { name: 'Alice' }]
-      //using JSON.parse(JSON.stringify())
-      const deepCopyArr = JSON.parse(JSON.stringify(oArr))
+//       const oArr = [1, 2, { name: 'Alice' }]
+//       //using JSON.parse(JSON.stringify())
+//       const deepCopyArr = JSON.parse(JSON.stringify(oArr))
 
-      deepCopyArr[2].name = 'James'
+//       deepCopyArr[2].name = 'James'
 
-      console.log(oArr) // Output: [1, 2, { name: "Alice" }]
-      console.log(deepCopyArr) // Output: [1, 2, { name: "James" }]
+//       console.log(oArr) // Output: [1, 2, { name: "Alice" }]
+//       console.log(deepCopyArr) // Output: [1, 2, { name: "James" }]
 
-      //using deep copy function
-      function deepCopy(arr) {
-        return arr.map((item) =>
-          Array.isArray(item)
-            ? deepCopy(item)
-            : typeof item === 'object'
-            ? { ...item }
-            : item
-        )
-      }
+//       //using deep copy function
+//       function deepCopy(arr) {
+//         return arr.map((item) =>
+//           Array.isArray(item)
+//             ? deepCopy(item)
+//             : typeof item === 'object'
+//             ? { ...item }
+//             : item
+//         )
+//       }
 
-      const deepCopyArray = deepCopy(oArr)
+//       const deepCopyArray = deepCopy(oArr)
 
-      deepCopyArray[2].name = 'Billy'
+//       deepCopyArray[2].name = 'Billy'
 
-      console.log(oArr) // Output: [1, 2, { name: "Alice" }]
-      console.log(deepCopyArray) // Output: [1, 2, { name: "Billy" }]
-    }
-  }
+//       console.log(oArr) // Output: [1, 2, { name: "Alice" }]
+//       console.log(deepCopyArray) // Output: [1, 2, { name: "Billy" }]
+//     }
+//   }
 
-  //Array() constructor
-  {
-    console.log('3.Array() constructor')
-    // Array() constructor
-    {
-      // Creating an array with a specified length
-      const arr = new Array(3) // Creates an array with 5 empty slots
-      console.log(arr) // Output: [ <3 empty items> ]
+//   //Array() constructor
+//   {
+//     console.log('3.Array() constructor')
+//     // Array() constructor
+//     {
+//       // Creating an array with a specified length
+//       const arr = new Array(3) // Creates an array with 5 empty slots
+//       console.log(arr) // Output: [ <3 empty items> ]
 
-      const arr2 = new Array(1, 2, 3) // Creates an array with 3 elements
-      console.log(arr2) // Output: [1, 2, 3]
-    }
-    // Array.of()
-    {
-      // Creating an array with a single numeric element
-      const arr3 = Array.of(3) // Creates an array with one element: 3
-      console.log(arr3) // Output: [3]
+//       const arr2 = new Array(1, 2, 3) // Creates an array with 3 elements
+//       console.log(arr2) // Output: [1, 2, 3]
+//     }
+//     // Array.of()
+//     {
+//       // Creating an array with a single numeric element
+//       const arr3 = Array.of(3) // Creates an array with one element: 3
+//       console.log(arr3) // Output: [3]
 
-      // Creating an array with multiple elements
-      const arr4 = Array.of(1, 2, 3) // Creates an array with the given elements
-      console.log(arr4) // Output: [1, 2, 3]
-    }
-    // Array.from()
-    {
-      // Creating an array from a string (iterable object)
-      const arr5 = Array.from('hello')
-      console.log(arr5) // Output: ['h', 'e', 'l', 'l', 'o']
+//       // Creating an array with multiple elements
+//       const arr4 = Array.of(1, 2, 3) // Creates an array with the given elements
+//       console.log(arr4) // Output: [1, 2, 3]
+//     }
+//     // Array.from()
+//     {
+//       // Creating an array from a string (iterable object)
+//       const arr5 = Array.from('hello')
+//       console.log(arr5) // Output: ['h', 'e', 'l', 'l', 'o']
 
-      // Creating an array from a Set (iterable object)
-      const set = new Set([1, 2, 3])
-      const arr6 = Array.from(set)
-      console.log(arr6) // Output: [1, 2, 3]
+//       // Creating an array from a Set (iterable object)
+//       const set = new Set([1, 2, 3])
+//       const arr6 = Array.from(set)
+//       console.log(arr6) // Output: [1, 2, 3]
 
-      // Using a mapping function to transform elements
-      const arr7 = Array.from([1, 2, 3], (x) => x * 2) // Multiplies each element by 2
-      console.log(arr7) // Output: [2, 4, 6]
+//       // Using a mapping function to transform elements
+//       const arr7 = Array.from([1, 2, 3], (x) => x * 2) // Multiplies each element by 2
+//       console.log(arr7) // Output: [2, 4, 6]
 
-      const arr8 = Array.from(4) //cannot create an array with a single numeric element
-      console.log(arr8) // Output: []
-    }
-  }
+//       const arr8 = Array.from(4) //cannot create an array with a single numeric element
+//       console.log(arr8) // Output: []
+//     }
+//   }
 
-  //destructuring
-  {
-    console.log('4.Destructuring')
+//   //destructuring
+//   {
+//     console.log('4.Destructuring')
 
-    const arr = [1, 2, 3, 4, 5]
+//     const arr = [1, 2, 3, 4, 5]
 
-    // Destructuring an array
-    const [first, second, ...rest] = arr
+//     // Destructuring an array
+//     const [first, second, ...rest] = arr
 
-    console.log(first) // Output: 1
-    console.log(second) // Output: 2
-    console.log(rest) // Output: [3, 4, 5]
+//     console.log(first) // Output: 1
+//     console.log(second) // Output: 2
+//     console.log(rest) // Output: [3, 4, 5]
 
-    // Destructuring an object
-    const user = { id: 1, name: 'Alice', age: 30 }
+//     // Destructuring an object
+//     const user = { id: 1, name: 'Alice', age: 30 }
 
-    const { id, name, age } = user
+//     const { id, name, age } = user
 
-    console.log(id) // Output: 1
-    console.log(name) // Output: Alice
-    console.log(age) // Output: 30
-  }
-}
+//     console.log(id) // Output: 1
+//     console.log(name) // Output: Alice
+//     console.log(age) // Output: 30
+//   }
+// }
 
 //Array practice
 {
@@ -274,14 +274,18 @@
   console.log('Array practice')
 
   //1. groupByParity
-  {
-    function groupByParity(arr) {
-      // Your logic here
-    }
+    {
+      function groupByParity(arr) {
+          return { even : arr.filter((x)=> x % 2 ==0 ) , 
+                   odd: arr.filter((x)=> x % 2 !==0)
 
-    const numbers = [1, 2, 3, 4, 5, 6]
-    console.log(groupByParity(numbers)) // Output: [[2, 4, 6], [1, 3, 5]]
-  }
+
+          }  
+      }
+
+      const numbers = [1, 2, 3, 4, 5, 6]
+      console.log(groupByParity(numbers)) // Output: [[2, 4, 6], [1, 3, 5]]
+    }
 
   //2. findMax element in an array
   {
