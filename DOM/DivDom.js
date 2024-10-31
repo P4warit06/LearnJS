d0ba20cacf0be8f879b99ec7dc501bd67b134ca5
Pawarit Samriang <pawarit.samriang@gmail.com>
2024-10-31 11:52:23 +0700

    Update DivDom

--- DOM/DivDom.js
+++ DOM/DivDom.js
@@ -6,3 +6,10 @@ for (let i = 0; i < divAttrs.length; i++) {
 console.log(divAttrs[i].name);
 console.log(divAttrs[i].length);
 }
+
+console.log(divElement.getAttribute('id')); // dom-01
+console.log(divElement.getAttribute('style')); //color :red
+// call attribute's  parent 
+console.log(divElement.attributes[0].parentElement) //null
+console.log(divElement.attributes[0].parentNode) //null 
+console.log(divElement.attributes[0].ownerElement) 
